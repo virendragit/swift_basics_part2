6c4f6cd52b5ba0b0130209e2f4e40accb1db4efe
Virendra Gupta <vgupta@ivycomptech.com>
2020-01-02 11:13:36 +0530

    added error handlin assertion and precondion

--- Swift_Basics_Part2.playground/Contents.swift
+++ Swift_Basics_Part2.playground/Contents.swift
@@ -1,5 +1,7 @@
 /* This playground contains explanation and code for the below topics in Swift 5.1
-1.Optional binding
+ 1.Optional binding
+ 2.Error Handling
+ 3.Assertion and Preconditions
  */
 
 import UIKit
@@ -50,3 +52,78 @@ if assumedString != nil {
 if let definedString = assumedString{
     print(definedString)
 }
+
+//Error Handling
+
+//run time
+
+func makeSandwich() throws {
+    // ..
+    
+}
+
+do {
+    try makeSandwich()
+    eatASandwich()
+    
+} catch SandwichError.outOfCleanDishes{
+    washDishes()
+} catch SanwichError.missingIngredients(let ingredients){
+    buyGroceries(ingredients)
+}
+
+
+//func eatASandwich() {
+////    print(<#T##items: Any...##Any#>)
+//}
+//
+//func washDish(){
+//
+//}
+//
+//func buyGroceries(ingredients){
+//
+//}
+
+//Assertions and Preconditions.
+
+/* Assertions and precondition used to express the assumption you make and the
+ expectation you have while coding.
+ 
+ Assertion help you find mistakes and incorrect assumptions during development,
+ and preconditions help you detect issue in production
+ 
+ Assertion and precondition aren't used for recoverable or expected errors.
+ 
+ The difference between assertions and precondition is in when they're checked:
+ Assertions are checked only in debug builds, but precondition are checked in both
+ debug and production builds.
+ 
+ In Production builds, the condition inside an assertion isn't evaluated,This means
+ you can use as many assertions as you want during your development process.without impacting performance in production.
+ 
+ */
+
+/* Debugging with Assertions*/
+
+let age = -3
+assert(age >= 0, "A person's age can't be less than zero.")
+
+//This assertion fails because -3 is not >= 0.
+
+// if the code already checks the condion , you use the assetionFailure function to indicate that an assetion has failed.
+
+if age > 10{
+    print("you can ride the roller-coaster")
+}
+else if age >= 0{
+    print ("you can ride the feeris wheel.")
+}
+else{
+    assertionFailure("A person's age can't be less than zero.")
+}
+
+//Enforcing Preconditions.
+
+precondition(index > 0, "Index must be greater than zero.")
+
